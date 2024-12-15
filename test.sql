376cec62837603572a61d2113336acef723f6d11
jiacai2050 <dev@liujiacai.net>
2024-12-15 21:37:24 +0800

    add badge

--- test.sql
+++ test.sql
@@ -5,6 +5,7 @@ CREATE VIRTUAL TABLE test_table
 USING vsag (dimension=3);
 
 insert into test_table(id, vec) values (1,'[1,2,3]');
-insert into test_table(vec, id) values ('[11,22,33]',2);
+insert into test_table(id, vec) values (2,'[11,22,33]');
+insert into test_table(id, vec) values (3,'[111,232,333]');
 
-SELECT * FROM test_table where vec match '[1,2,4]';
+SELECT id,distance FROM test_table where vec match '[1,2,4]';
